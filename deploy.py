cdb0fbb7a1a83d2cadfc2f6ad06e4501383459ec
ivan <elivanse@hotmail.com>
2022-03-06 22:55:19 -0300

    a

---
+++ deploy.py
@@ -0,0 +1,30 @@
+import json
+
+from solcx import compile_standard, install_solc
+
+install_solc("0.6.0")
+
+with open("SimpleStorage.sol", "r") as file:
+    simple_storage_file = file.read()
+
+# compile our solidity
+
+compiled_sol = compile_standard(
+    {
+        "language": "Solidity",
+        "sources": {
+            "SimpleStorage.sol": {
+                "content": simple_storage_file}},
+        "settings": {
+            "outputSelection": {
+                "*": {"*": ["abi", "metadata", "evm.bytecode", "evm.sourceMap"]}
+
+            }
+        },
+    },
+    solc_version="0.6.0",
+)
+with open("compiled_code.json", "w") as file:
+    json.dump(compiled_sol, file)
+# print(compiled_sol)
+bytecode = compiled_sol["contracts"]["SimpleStorage.sol"]["SimpleStorage"]["evm"]["bytecode"]["object"]
