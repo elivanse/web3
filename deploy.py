ad9edafb4ab74604b47516f17f93f8fa659db814
ivan <elivanse@hotmail.com>
2022-03-09 22:08:38 -0300

    pip install web3

--- deploy.py
+++ deploy.py
@@ -1,6 +1,7 @@
 import json
 
 from solcx import compile_standard, install_solc
+from web3 import Web3
 
 install_solc("0.6.0")
 
@@ -27,4 +28,19 @@ compiled_sol = compile_standard(
 with open("compiled_code.json", "w") as file:
     json.dump(compiled_sol, file)
 # print(compiled_sol)
+
+# get bytecode
 bytecode = compiled_sol["contracts"]["SimpleStorage.sol"]["SimpleStorage"]["evm"]["bytecode"]["object"]
+
+# get abi
+abi = compiled_sol["contracts"]["SimpleStorage.sol"]["SimpleStorage"]["abi"]
+
+# web3
+w3 = Web3(Web3.HTTPProvider("http://127.0.0.1:8545"))
+chain_id = 1337
+my_address = "0xa8208f632e2Aa80C3A876846B6f005b5Ca3E1923"
+private_key = "0x14fd541dbb7599122a68cac690d05cdfbc84e0c864a3f357d5103953dfd345fb"
+
+# Create the contract in Python
+SimpleStorage = w3.eth.contract(abi=abi, bytecode=bytecode)
+print(SimpleStorage)
